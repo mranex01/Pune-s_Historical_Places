7d228811738d6587aa4e5d7c331a933e14e25d75
Aniket <aniket@Anikets-MacBook-Air.local>
2023-08-17 22:15:12 +0530

    punes historical places

---
+++ js/video-list.js
@@ -0,0 +1,47 @@
+let allVideos = [
+   {
+      name: "Example 1",
+      src: "1",
+      id: "vid_1"
+   },
+   {
+      name: "Button Hover Effect",
+      src: "2",
+      id: "vid_2"
+   },
+   {
+      name: "Confirm Password using Html CSS & js",
+      src: "3",
+      id: "vid_3"
+   },
+   {
+      name: "Creative Card Hover Effect",
+      src: "Creative Card Hover Effect ",
+      id: "vid_4"
+   },
+   {
+      name: "Creative Digital Clock Ui Design",
+      src: "Creative Digital Clock Ui Design - Html Css & Vanilla Javascript-1",
+      id: "vid_5"
+   },
+   {
+      name: "Creative Our Team Section",
+      src: "Creative Our Team Section Using HTML & CSS Neumorphism",
+      id: "vid_6"
+   },
+   {
+      name: "Filter Text Animation Using Html and CSS",
+      src: "Filter Text Animation Using Htm and Pure CSS",
+      id: "vid_7"
+   },
+   {
+      name: "Glassmorphism Calculater New Design",
+      src: "Glassmorphism Calculater New Design Html",
+      id: "vid_8"
+   },
+   {
+      name: "How To  Make Cheatsheet",
+      src: "How To  Make Cheatsheet Using Html, CSS and Javascript",
+      id: "vid_9"
+   }
+]
